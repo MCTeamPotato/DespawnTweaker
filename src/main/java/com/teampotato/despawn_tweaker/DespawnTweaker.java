12f0a4008f7a0c6d9c90cb03cafd6445da642f81
Kasualix <670067575@qq.com>
2023-08-22 01:22:44 +0800

    sync

--- src/main/java/com/teampotato/despawn_tweaker/DespawnTweaker.java
+++ src/main/java/com/teampotato/despawn_tweaker/DespawnTweaker.java
@@ -18,16 +18,23 @@ public class DespawnTweaker {
         MinecraftForge.EVENT_BUS.register(SpawnChecker.class);
     }
     public static ForgeConfigSpec configSpec;
+
+    public static final ForgeConfigSpec.BooleanValue allowMobsSpawnedBySpawnersToDespawn;
     public static ForgeConfigSpec.ConfigValue<List<? extends String>> structuresMods;
     public static ForgeConfigSpec.ConfigValue<List<? extends String>> structures;
+
+    public static ForgeConfigSpec.BooleanValue enableLetMeDespawnOptimization;
 
     static {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
         builder.push("DespawnTweaker");
+        allowMobsSpawnedBySpawnersToDespawn = builder.comment("Turn this off to allow the despawn of mobs spawned by spawners").define("allowMobsSpawnedBySpawnersToDespawn", false);
         structuresMods = builder.comment("If you add modIDs to this list, only mobs in the structures of the mods will be affected by DespawnTweaker.").defineList("StructuresMods", Lists.newArrayList(), o -> o instanceof String);
         structures = builder.comment("If you add sturctures registry names to list, only mobs in the structures will be affected by DespawnTweaker.", "However, this can be combined with StructuresMods").defineList("Structures", Lists.newArrayList(), o -> o instanceof String);
+        builder.pop();
+        builder.push("Optimization");
+        enableLetMeDespawnOptimization = builder.comment("DespawnTweaker does contain the optimization of Let Me Despawn mod and resolves its potenial performance issue on equipments drop of despawning.", "Turn this off to disable the optimization").define("enableLetMeDespawnOptimization", true);
         builder.pop();
         configSpec = builder.build();
     }
 }
-
--- src/main/java/com/teampotato/despawn_tweaker/mixin/MobMixin.java
+++ src/main/java/com/teampotato/despawn_tweaker/mixin/MobMixin.java
@@ -1,16 +1,62 @@
 package com.teampotato.despawn_tweaker.mixin;
 
 import com.google.common.collect.Sets;
+import com.teampotato.despawn_tweaker.DespawnTweaker;
 import com.teampotato.despawn_tweaker.api.IMob;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.feature.StructureFeature;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Set;
 
 @Mixin(Mob.class)
-public abstract class MobMixin implements IMob {
+public abstract class MobMixin extends LivingEntity implements IMob {
+    @Shadow private boolean persistenceRequired;
+    @Unique
+    private boolean despawnTweaker$pickedItems = false;
+
+    protected MobMixin(EntityType<? extends LivingEntity> arg, Level arg2) {
+        super(arg, arg2);
+    }
+
+    @Inject(method = "setItemSlotAndDropWhenKilled", at = @At("TAIL"))
+    private void onSet(EquipmentSlot arg, ItemStack arg2, CallbackInfo ci) {
+        if (DespawnTweaker.enableLetMeDespawnOptimization.get()) {
+            this.despawnTweaker$pickedItems = true;
+            this.persistenceRequired = this.hasCustomName();
+        }
+    }
+
+    @Inject(method = "checkDespawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Mob;remove()V", shift = At.Shift.AFTER))
+    private void onDespawn(CallbackInfo ci) {
+        if (DespawnTweaker.enableLetMeDespawnOptimization.get() && this.despawnTweaker$pickedItems) this.despawnTweaker$dropEquipmentOnDespawn();
+    }
+
+    @Unique
+    private static final EquipmentSlot[] despawnTweaker$slots = EquipmentSlot.values();
+
+    @Unique
+    private void despawnTweaker$dropEquipmentOnDespawn() {
+        for (EquipmentSlot equipmentSlot : despawnTweaker$slots) {
+            ItemStack itemStack = this.getItemBySlot(equipmentSlot);
+            CompoundTag tag = itemStack.getTag();
+            if (!itemStack.isEmpty() && !(tag != null && tag.toString().contains("vanishing_curse"))) {
+                this.spawnAtLocation(itemStack);
+                this.setItemSlot(equipmentSlot, ItemStack.EMPTY);
+            }
+        }
+    }
     @Unique
     private Set<StructureFeature<?>> despawnTweaker$spawnStructures = Sets.newHashSet();
 
